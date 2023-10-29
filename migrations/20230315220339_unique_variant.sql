1608af55d688f221b9f1d5db22116f986e2c5842
mingxiaowei <mingxiaowei@berkeley.edu>
2023-10-29 16:52:24 -0700

    idk

--- migrations/20230315220339_unique_variant.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE questions
-ADD COLUMN IF NOT EXISTS unique_variants_enabled BOOLEAN DEFAULT FALSE;
-
-ALTER TABLE questions
-ADD COLUMN IF NOT EXISTS unique_variants_max integer DEFAULT 1;
